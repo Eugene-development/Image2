c8b836111a3d9d54003940357d7a391207193b71
indexpro24@gmail.com <indexpro24@gmail.com>
2021-06-14 10:35:24 +0300

    master

--- src/app/Http/Controllers/ImageController.php
+++ src/app/Http/Controllers/ImageController.php
@@ -12,28 +12,86 @@ class ImageController extends Controller
 {
     public function store(Request $request)
     {
-        $path = $request->file('image')->store('images', 's3');
 
-        //        Или ткут паблик делать, или в конфигах s3 добавить 'visibility' => 'public', или на самом aws
-        Storage::disk('s3')->setVisibility($path, 'public');
+        $token = $request->bearerToken();
 
-        $image = Image::create([
-            'filename' => basename($path),
-            'url' => Storage::disk('s3')->url($path),
-            'product_id' => $request->file('image')->getClientOriginalName()
-        ]);
+        if($token == '1'){
+            $path = $request->file('image')->store('images', 's3');
 
-        return $image;
+            //        Или ткут паблик делать, или в конфигах s3 добавить 'visibility' => 'public', или на самом aws
+            Storage::disk('s3')->setVisibility($path, 'public');
+
+            $image = Image::create([
+                'project_id' => $request->bearerToken(),
+                'filename' => basename($path),
+                'url' => Storage::disk('s3')->url($path),
+                'tagable_id' => $request->file('image')->getClientOriginalName()
+            ]);
+
+            return $image;
+        }
+
+        if($token == '2'){
+            $path = $request->file('image')->store('luba-mebel/products', 's3');
+
+            //        Или ткут паблик делать, или в конфигах s3 добавить 'visibility' => 'public', или на самом aws
+            Storage::disk('s3')->setVisibility($path, 'public');
+
+            $image = Image::create([
+                'project_id' => $request->bearerToken(),
+                'filename' => basename($path),
+                'url' => Storage::disk('s3')->url($path),
+                'tagable_id' => $request->file('image')->getClientOriginalName()
+            ]);
+
+            return $image;
+        }
+
+        if($token == '3'){
+            $path = $request->file('image')->store('mebel-mobile/products', 's3');
+
+            //        Или ткут паблик делать, или в конфигах s3 добавить 'visibility' => 'public', или на самом aws
+            Storage::disk('s3')->setVisibility($path, 'public');
+
+            $image = Image::create([
+                'project_id' => $request->bearerToken(),
+                'filename' => basename($path),
+                'url' => Storage::disk('s3')->url($path),
+                'tagable_id' => $request->file('image')->getClientOriginalName()
+            ]);
+
+            return $image;
+        }
+
+
+
+
+
+
     }
 
-    public function show(Image $image)
+    public function show(Request $request, Image $image) //TODO этот метод вообще нужен?
     {
-        return Storage::disk('s3')->response('images/' . $image->filename);
+        $token = $request->bearerToken();
+
+        if($token == '7') {
+            return Storage::disk('s3')->response('images/' . $image->filename);
+        }
+
+        if($token == '2') {
+            return Storage::disk('s3')->response('luba-mebel/products' . $image->filename);
+        }
+
+        if($token == '3') {
+            return Storage::disk('s3')->response('mebel-mobile/products' . $image->filename);
+        }
+
     }
 
-    public function delete($param)
+    public function delete(Request $request, $param)
     {
-        Image::where('product_id', $param)
+        Image::where('project_id', $request->bearerToken())
+            ->where('tagable_id', $param)
             ->delete();
     }
 }
